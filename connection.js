52c03d8497f429c928502a41200b737c218a696e
LuigiNettoB <162366361+LuigiNettoB@users.noreply.github.com>
2024-10-29 17:20:34 -0300

    Adição e conexão com MongoDB

---
+++ connection.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const connect = () => {
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@matematicametro.lf0ks.mongodb.net/matematicametro?retryWrites=true&w=majority&appName=MatematicaMetro`)
+
+    const connection = mongoose.connection
+
+    connection.on("error", () => {
+        console.error("Erro ao conectar com o MongoDB")
+    })
+
+    connection.on("open", () => {
+        console.log("Conectado ao MongoDB com Sucesso!")
+    })
+}
+
+connect()
+
+module.exports = mongoose
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Hello World!")
+
+const express = require("express")
+const cors = require("cors")
+
+require("dotenv").config()
+
+var app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Servidor rodando na Porta ${port}`)
+})
+
+require("./connection")
